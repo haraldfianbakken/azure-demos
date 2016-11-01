5f2bd204166bf8914c06c9940c7fe8caf5a5c3a7
Harald S. Fianbakken <Harald S. Fianbakken>
2016-11-01 23:20:25 +0100

    Updated demo script

---
+++ MSOpenSourceAndCloud2016/Demo.md
@@ -0,0 +1,36 @@
+# Pre reqs
+
+- Windows 10 or WMF5 installed on your machine.
+- Following modules installed:
+ - AzureRM
+
+## Resource groups (1 min)
+ - Create RG
+ - Create SA
+
+## RBAC (5 min)
+- Show users
+- Show roles 
+- Show restrictions
+
+## Resource locks (3 min)
+- Show resource locks 
+- Write/Delete locks
+
+## Secrets & Key vault (3 min)
+- 
+
+## Azure templates (3 min)
+- Azure templates
+- Azure 
+
+
+## Automation & DSC (5 min)
+- Create automation account
+- Show simple DSC 
+- Create VM (might take time)
+- Onboard VM 
+
+
+
+
---
+++ MSOpenSourceAndCloud2016/Demo.ps1
@@ -0,0 +1,72 @@
+Select-AzureRmSubscription -SubscriptionName 'Visual Studio Premium med MSDN'
+$resourceGroupname = 'opensourcecloudevening2016';
+$storageAccountName = "hsfmydemostorageaccount";
+$storageAccountName2 = "hsfmydemostorageaccount2";
+$rg = New-AzureRmResourceGroup -Name $resourceGroupname -Location 'West Europe';
+$sa = New-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupname -SkuName Standard_LRS -Location 'West europe' -Kind Storage ;
+$sa2 = New-AzureRmStorageAccount -Name $storageAccountName2 -ResourceGroupName $resourceGroupname -SkuName Standard_LRS -Location 'West europe' -Kind Storage ;
+
+
+# Login with second user 
+Get-AzureRMResourceGroup
+# Back to master user
+Get-AzureRmRoleDefinition 
+Get-AzureRmRoleAssignment -ResourceGroupName $resourceGroupname;
+
+$role=New-AzureRmRoleAssignment -SignInName 'haralddev@blabla.onmicrosoft.com' -Scope $rg.ResourceId -RoleDefinitionName 'reader'
+
+
+# Back to Other user
+# Show resource group
+
+# Back to master user
+# Remove roles 
+$role|Remove-AzureRmRoleAssignment ;
+$role=New-AzureRmRoleAssignment -SignInName 'haralddev@blabla.onmicrosoft.com' -Scope $rg.ResourceId -RoleDefinitionName 'contributor'
+
+# Create a lock
+New-AzureRmResourceLock -LockName 'DoNotDeleteStorageAccount' -LockLevel CanNotDelete -Scope $sa.Id;
+
+# Check that contributor is unable to delete this storage account (other account)
+
+
+# Create a readonly lock (master account)
+
+New-AzureRmResourceLock -LockName Readonly -Scope $rg.ResourceId -LockLevel ReadOnly;
+
+# See that you're unable to create resources in this RG now
+$sa2 = New-AzureRmStorageAccount -Name $storageAccountName2 -ResourceGroupName $resourceGroupname -SkuName Standard_LRS -Location 'West europe' -Kind Storage ;
+# fails
+
+# Clean up
+Remove-AzureRmResourceLock -LockName readonly -Scope $rg.ResourceId;
+
+# Key vault
+$kv=Get-AzureRmKeyVault -VaultName 'MyTest' -ResourceGroupName $resourceGroupname 
+$kv=New-AzureRmKeyVault -VaultName 'MyTestOSDemo' -ResourceGroupName $resourceGroupname -Location 'West europe'
+
+$secret = Set-AzureKeyVaultSecret -VaultName $kv.VaultName -Name 'Mysecret' -SecretValue ("Mysecret"|ConvertTo-SecureString -AsPlainText -Force) 
+
+
+$secretFetch = Get-AzureKeyVaultSecret -VaultName $kv.VaultName -Name MySecret 
+
+# ArmViz
+
+# Start group deployment
+AzureRM\New-AzureRmResourceGroupDeployment -Name "demodeployment$((Get-date).ToString("DDMMyyyy:hhii"))" -ResourceGroupName $resourceGroupname -Mode Incremental -TemplateParameterFile .\Templates\Deploy.param.json -TemplateFile .\Templates\Deploy.json
+
+
+$amAccount = New-AzureRmAutomationAccount -ResourceGroupName $resourceGroupname -Name 'opensourceautomationaccount' -Location 'west europe' -Plan Free; 
+
+
+Import-AzureRmAutomationDscConfiguration -SourcePath C:\git\haraldfianbakken\azure-demo\MSOpenSourceAndCloud2016\DSC\FirstConfiguration.ps1 -force -Published -Verbose -AutomationAccountName 
+
+$job = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $resourceGroupname -ConfigurationName 'FirstConfiguration' -Parameters @{} -ConfigurationData (Import-PowerShellDataFile .\DSC\DataFile.psd1) -AutomationAccountName $amAccount.AutomationAccountName -Verbose 
+
+$job|Get-AzureRmAutomationDscCompilationJob;
+
+# Show onboarding of VM
+
+# Clean up the demo
+
+Remove-AzureRmResource $resourceGroupname -Force
